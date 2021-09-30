1e40f64714ce99ed679829ed368abfdb03bb6c7d
filip-dobrocky <filip.dobrocky@gmail.com>
2021-09-30 10:57:32 +0200

    cv02 extIT, LED blink

--- cv02/Src/main.c
+++ cv02/Src/main.c
@@ -24,7 +24,11 @@
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
-void configure_GPIO()
+#define LED_TIME_BLINK 300
+
+volatile uint32_t Tick;
+
+void configure_GPIO(void)
 {
 	RCC->AHBENR |= RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN | RCC_AHBENR_GPIOCEN; // enable
 	GPIOA->MODER |= GPIO_MODER_MODER4_0; // LED1 = PA4, output
@@ -33,13 +37,51 @@ void configure_GPIO()
 	GPIOC->PUPDR |= GPIO_PUPDR_PUPDR1_0; // S1 = PC1, pullup
 }
 
+void configure_external_it(void)
+{
+	RCC->APB2ENR |= RCC_APB2ENR_SYSCFGEN;
+	SYSCFG->EXTICR[0] |= SYSCFG_EXTICR1_EXTI0_PC; // select PC0 for EXTI0
+	EXTI->IMR |= EXTI_IMR_MR0; // mask
+	EXTI->FTSR |= EXTI_FTSR_TR0; // trigger on falling edge
+	NVIC_EnableIRQ(EXTI0_1_IRQn); // enable EXTI0_1
+}
+
+void EXTI0_1_IRQHandler(void)
+{
+	if (EXTI->PR & EXTI_PR_PR0) { // check line 0 has triggered the IT
+		EXTI->PR |= EXTI_PR_PR0; // clear the pending bit
+		GPIOB->ODR ^= (1 << 0);
+	}
+}
+
+void SysTick_Handler(void)
+{
+	Tick++;
+}
+
+void blikac(void)
+{
+	static uint32_t delay;
+
+	if (Tick > delay + LED_TIME_BLINK) {
+		GPIOA->ODR ^= (1 << 4);
+		delay = Tick;
+	}
+}
+
+
+
 int main(void)
 {
 	configure_GPIO();
+	configure_external_it();
+
+	SysTick_Config(8000); // 1ms
+
 
     /* Loop forever */
 	for(;;)
 	{
-
+		blikac();
 	}
 }

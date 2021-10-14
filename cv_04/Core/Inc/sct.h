fd71cc1e66c9ac233178dce2829796099313a217
filip-dobrocky <filip.dobrocky@gmail.com>
2021-10-14 13:01:15 +0200

    cv 04

---
+++ cv_04/Core/Inc/sct.h
@@ -0,0 +1,18 @@
+/*
+ * sct.h
+ *
+ *  Created on: 7. 10. 2021
+ *      Author: student
+ */
+
+#ifndef SCT_H_
+#define SCT_H_
+
+#include <stdint.h>
+
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value, uint8_t led);
+
+#endif /* SCT_H_ */
